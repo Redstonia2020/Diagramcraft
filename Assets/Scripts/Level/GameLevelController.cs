e3e85a79996d6941a70b2a7d4e02ed04bcedfe14
Redstonia <redstonia1234@gmail.com>
2022-06-01 17:54:20 +1200

    basic save data functionality

---
+++ Assets/Scripts/Level/GameLevelController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLevelController : MonoBehaviour
+{
+    void Start()
+    {
+        SaveDataManagement.Load();
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SaveDataManagement/SaveDataManagement.cs
+++ Assets/Scripts/SaveDataManagement/SaveDataManagement.cs
@@ -6,10 +6,33 @@ using UnityEngine;
 
 public static class SaveDataManagement
 {
+    public readonly static string SaveLocation = "saves/";
+    public static string SaveFile = "";
+    public static string SaveDirectory
+    {
+        get
+        {
+            return SaveLocation + SaveFile;
+        }
+        
+        set { }
+    }
+
+    public static void Load()
+    {
+        CreateSaveDirectory();
+    }
+
     public static void Save()
     {
+        if (SaveFile == "")
+        {
+            SaveFile = "autosave.json";
+        }
+
         SaveData data = CreateSaveData();
-        Debug.Log(JsonUtility.ToJson(data));
+        string jsonData = JsonUtility.ToJson(data);
+        File.WriteAllText(SaveDirectory, jsonData);
     }
 
     private static SaveData CreateSaveData()
@@ -19,10 +42,18 @@ public static class SaveDataManagement
             Layout = LevelLayout.Layout
         };
     }
+
+    private static void CreateSaveDirectory()
+    {
+        if (!Directory.Exists(SaveLocation))
+        {
+            Directory.CreateDirectory(SaveLocation);
+        }
+    }
 }
 
 [Serializable]
 public class SaveData
 {
-    public List<Tile> Layout { get; set; }
+    public List<Tile> Layout;
 }
