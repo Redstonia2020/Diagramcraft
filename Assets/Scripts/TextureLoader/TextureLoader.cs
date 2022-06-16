88f79998384a4b2eeccffeafe9470cdf942600ca
Redstonia <redstonia1234@gmail.com>
2022-06-16 17:50:45 +1200

    - added base texture grouping and texture class storage... thingies.. whatever - started to compile a list of texture groupings

--- Assets/Scripts/TextureLoader/TextureLoader.cs
+++ Assets/Scripts/TextureLoader/TextureLoader.cs
@@ -10,4 +10,39 @@ public static class TextureLoader
     {
         return References.Sprites[id];
     }
+
+    public static List<TextureGroup> TextureGroups = new List<TextureGroup>
+    {
+        new TextureGroup("Acacia Door", ("Top", "acacia_door_top"), ("Bottom", "acacia_door_bottom")),
+        new TextureGroup("Acacia Log", ("Side", "acacia_log"), ("Top", "acacia_log_top")),
+        new TextureGroup("Activator Rail", ("Off", "activator_rail"), ("On", "activator_rail_on")),
+        new TextureGroup("Ancient Debris", ("Top", "ancient_debris_top"), ("Side", "ancient_debris_side")),
+        new TextureGroup("Anvil", ("Texture", "anvil"), ("Top", "anvil_top")),
+        //azalea plant
+    };
+}
+
+public class TextureGroup
+{
+    public string Name;
+    public List<Texture> Textures;
+
+    public TextureGroup(string name, params (string name, string file)[] textures)
+    {
+        Name = name;
+        foreach (var t in textures)
+        {
+            Textures.Add(new Texture
+            {
+                Name = t.name,
+                Sprite = Resources.Load<Sprite>(t.file)
+            });
+        }
+    }
+}
+
+public class Texture
+{
+    public string Name;
+    public Sprite Sprite;
 }
